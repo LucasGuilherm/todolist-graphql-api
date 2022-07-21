4d4c9f8287685350c678a4fceb91b67ca28a7ba1
LucasGuilherm <lucassade110@gmail.com>
2022-07-21 00:42:37 -0300

    Primeiro Commit

---
+++ index.js
@@ -0,0 +1,86 @@
+// Firebase
+import admin from "firebase-admin";
+
+// const serviceAccount = require("./firebase-account.json");
+import serviceAccount from "./firebase-account.json" assert { type: "json" };
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+// Apollo Server
+import {
+  ApolloServer,
+  ApolloError,
+  ValidationError,
+  gql,
+  UserInputError,
+} from "apollo-server";
+
+const typeDefs = gql`
+  type Item {
+    tittle: String
+    description: String
+    isCompleted: Boolean
+  }
+
+  type Query {
+    items(isCompleted: Boolean, tittle: String): [Item]
+  }
+
+  input Filter {
+    tittle: String
+    description: String
+    isCompleted: Boolean
+  }
+
+  type Mutation {
+    createItem(itemProps: Filter): Item
+  }
+`;
+
+const resolvers = {
+  Query: {
+    async items(_, { isCompleted, tittle }) {
+      const itemsRef = admin.firestore().collection("items");
+
+      let items = itemsRef;
+
+      if (typeof isCompleted != "undefined") {
+        items = items.where("isCompleted", "==", isCompleted);
+      }
+
+      if (typeof tittle != "undefined") {
+        items = items.where("tittle", "==", tittle);
+      }
+
+      items = await items.get();
+      return items.docs.map((item) => item.data());
+    },
+  },
+  Mutation: {
+    async createItem(_, { itemProps }) {
+      const itemsRef = admin.firestore().collection("items");
+
+      try {
+        await itemsRef.add(itemProps);
+      } catch (error) {
+        throw new UserInputError(error);
+      }
+
+      return itemProps;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  csrfPrevention: true,
+  cache: "bounded",
+});
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
